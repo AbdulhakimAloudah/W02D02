b23099ba880545de2ea37a2e42e57b3f474b55c1
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-18 16:58:53 +0300

    ks

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
+//function adddOne(arr) {}
+
 //--1
 const addOne = (arr) => {
-    let res = []
-arr.forEach(function(item,index){
-    res.push(item+1)
-})
-console.log();
-return res
+  let res = [];
+  arr.forEach(function (item) {
+    res.push(item + 1);
+  });
+  //console.log(8);
+  return res;
 };
 
 //---2
+const addExclamation = (arr) => {
+  let res = [];
+  arr.forEach(function (item) {
+    res.push(item + "!");
+  });
+  console.log(one);
+  return res;
+};
+
+//-- 3
+const addOneByMap = (arr) => {
+  let res = arr.map(function (item) {
+    return item + 1;
+  });
+  return res;
+};
+//--4
+const addQuestion = (arr) => {
+  let res = arr.map(function (item) {
+    return item + "?";
+  });
+  return res;
+};
+
+//--5
+const forLoopTwoToThe = (arr) => {
+  let res = arr.map(function (item) {
+    return Math.pow(item, 2);
+  });
+  return res;
+};
+
+//--6
+const typeNum = (arr) => {
+  let res = arr.filter(function (item) {
+    return typeof item === "number";
+  });
+  return res;
+
+  //---7
+};
